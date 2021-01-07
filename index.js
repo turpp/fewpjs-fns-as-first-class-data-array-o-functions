382cbc93731470295319664f9136ae84b25fdd5f
turpp <white.tripp@icloud.com>
2021-01-07 12:22:02 -0600

    Done.

---
+++ index.js
@@ -0,0 +1,48 @@
+function wakeDog(dogName, dogBreed){
+    let string = `Wake ${dogName} the ${dogBreed}`
+    console.log(string);
+    return string;
+}
+
+function leashDog(dogName, dogBreed){
+    let string = `Leash ${dogName} the ${dogBreed}`;
+    console.log(string);
+    return string;
+}
+
+function walkToPark(dogName, dogBreed) {
+    let string = `Walk to the park with ${dogName} the ${dogBreed}`;
+    console.log(string);
+    return string;
+}
+
+function throwFrisbee(dogName, dogBreed){
+    let string = `Throw the frisbee for ${dogName} the ${dogBreed}`
+    console.log(string)
+    return string
+}
+
+function walkHome(dogName, dogBreed) {
+    let string = `Walk home with ${dogName} the ${dogBreed}`
+    console.log(string)
+    return string
+}
+
+function unleashDog(dogName, dogBreed) {
+    let string = `Unleash ${dogName} the ${dogBreed}`
+    console.log(string)
+    return string
+}
+
+let routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog]
+
+function exerciseDog(dogName, dogBreed) {
+    let array=[]
+    for (i=0; i<routine.length; i++) {
+        array.push(routine[i](dogName, dogBreed))
+    }
+
+    
+    return array
+}
+let i=0
